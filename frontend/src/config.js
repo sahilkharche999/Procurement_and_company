186d38c60b024cfeb0c82836dfc95b88e07e496b
Ashwin Gorle <ashwin.g@consultadd.com>
2026-03-20 20:03:20 +0530

    fix : deployment server path

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,5 +1,5 @@
-// const RAW_SERVER_URL = import.meta.env.VITE_SERVER_URL || "http://18.217.140.21:8000/";
-const RAW_SERVER_URL = import.meta.env.VITE_SERVER_URL || "http://localhost:8000/";
+const RAW_SERVER_URL = import.meta.env.VITE_SERVER_URL || "http://18.217.140.21:8000/";
+// const RAW_SERVER_URL = import.meta.env.VITE_SERVER_URL || "http://localhost:8000/";
 
 export const SERVER_URL = RAW_SERVER_URL.endsWith("/")
   ? RAW_SERVER_URL
